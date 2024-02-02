ae28609cc5a8e2cb10a5b11ebbc47fd0f1c940c3
skyDe <sky_de1991@outlook.com>
2024-02-01 23:39:23 -0800

    fist commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "blue" }}>This is my first react native App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "red",
+    borderRadius: 35,
+  },
+});
